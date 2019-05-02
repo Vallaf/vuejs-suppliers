4eee9c1e515a8edec9467fa99a0a8766f12e9253
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-05-02 17:06:06 +0200

    6 pas fini

--- spiruline_project/src/components/Supplier.vue
+++ spiruline_project/src/components/Supplier.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="Supplier">
     <h1>Fournisseur : {{name}}</h1>
-    <h3>A du stock ?  </h3>
-   <h3 :class="{'text-success':status, 'text-danger':!status}">{{status ? "OK" : "KO"}}</h3>
-  
-    
+    <h3
+      :class="{'text-success':status, 'text-danger':!status}"
+    >A du stock ? {{status ? "OK" : "KO"}}</h3>
     <h5>Date de dernier relevé des stocks : {{checkedAt}}</h5>
   </div>
 </template>
 
 <script>
 export default {
- 
-
-  data() {
-       return {
-      name: "A Spiruline",
-      status: false,
-      checkedAt: new Date(),   
-    }
+  props: {
+    name: String,
+    status: Boolean,
+    checkedAt: String
   }
 };
 </script>
--- spiruline_project/src/components/SuppliersList.vue
+++ spiruline_project/src/components/SuppliersList.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="SuppliersList">
-    <!-- <router-link to="/supplier">Fournisseur A</router-link> -->
-
-    <Supplier></Supplier>
+    <router-link to="/supplier"></router-link>
+    <h1>Liste des fournisseurs</h1>
+    <Supplier
+      v-for="supplier in suppliers"
+      :key="supplier.id"
+      :name="supplier.name"
+      :status="supplier.status"
+      :checkedAt="supplier.checkedAt"
+    ></Supplier>
   </div>
 </template>
 
@@ -10,10 +16,30 @@
 import Supplier from "./Supplier.vue";
 export default {
   name: "SuppliersList",
+
   components: {
     Supplier
   },
-  data() {}
+
+  data() {
+    activeColor: "red";
+    return {
+      suppliers: [
+        {
+          id: 1,
+          name: "Fournisseur 1 ",
+          status: true,
+          checkedAt: new Date()
+        },
+        {
+          id: 2,
+          name: "Fournisseur 2",
+          status: false,
+          checkedAt: new Date()
+        }
+      ]
+    };
+  }
 };
 </script>
 
@@ -30,5 +56,4 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-
 </style>
